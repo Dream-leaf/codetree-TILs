cf3632ede96c18d2cd4c3afd2b2dd2242d7c659a
Dream-leaf <44544326+Dream-leaf@users.noreply.github.com>
2023-11-12 22:49:12 +0900

    Title: 변수 선언하기, Time: 77ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231112/변수 선언하기/declaring-variables.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int a;
+    static char b;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        a = 3;
+        b = 'C';
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
