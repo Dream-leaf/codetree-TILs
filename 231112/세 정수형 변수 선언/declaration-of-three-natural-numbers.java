5f489efd468a25edb57484224977d14ab6477b39
Dream-leaf <44544326+Dream-leaf@users.noreply.github.com>
2023-11-12 22:51:29 +0900

    Title: 세 정수형 변수 선언, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231112/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        int a, b, c;
+        a = 7; b = 23; c = 30;
+        System.out.println(a + " + " + b + " = " + c);
+    }
+}
